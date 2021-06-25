b54fe81325411f3c2fa321aebfe0e6e902eaee68
kanav.ghai <kanav.ghai@publicissapient.com>
2021-06-25 15:51:34 +0530

    Question object modified

--- codebase/miniproject/core/src/main/java/com/miniproject/core/entity/Question.java
+++ codebase/miniproject/core/src/main/java/com/miniproject/core/entity/Question.java
@@ -1,5 +1,7 @@
 package com.miniproject.core.entity;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +13,7 @@ public class Question {
 	private Integer qid;
 	private Integer uid;
 	private String qBody;
+	private Integer bestAnswerId;
+	private List<Integer> answers;
 	
 }
